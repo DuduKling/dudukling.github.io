9698b0bb641a517c644cd2f06dff0579c825a152
Eduardo Kling <eduardokmesiano@gmail.com>
2017-10-26 23:24:07 -0200

    Added a kind of lightbox to see Portfolio images in a larger scale.

--- dev/default.js
+++ dev/default.js
@@ -3,12 +3,16 @@ document.addEventListener("scroll", function(){
     MenuChangeOnScroll();
 });
 
-// Get skills li
+/* Function to calculate position of a element referent to the page. */
+function offset(el) {
+    let rect = el.getBoundingClientRect(),
+    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    return {top: rect.top + scrollTop}
+}
+
+/* Animate Skills bars to expand only when the user screen is showing them. */
 let level = document.querySelectorAll(".level");
-
-// Get window size.
 let tela = window.innerHeight;
-
 let lis = document.getElementsByClassName("level");
 let liTopPositions = new Array();
 let liBotPositions = new Array();
@@ -19,14 +23,7 @@ level.forEach(function(e, i) {
     liBotPositions.push(liTop.top + 26);
 }, this);
 
-function offset(el) {
-    let rect = el.getBoundingClientRect(),
-    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    return {top: rect.top + scrollTop}
-}
-
 function SkillsAnimationOnScroll() {
-    // Get user's window position relative to page.
     let topoTela = document.documentElement.scrollTop;
     let baixoTela = Number(topoTela+tela);
     
@@ -41,7 +38,7 @@ function SkillsAnimationOnScroll() {
     }, this);
 }
 
-
+/* Change style of Menu when user scroll down after the ATF (Above The Fold). */
 let about = document.querySelectorAll(".about");
 let menu = document.querySelectorAll(".menu");
 let aboutTop = offset(about[0]);
@@ -57,3 +54,38 @@ function MenuChangeOnScroll(){
         menu[0].classList.add("-top");
     }
 }
+
+
+/* A shitty kind of Lightbox */
+let body = document.querySelectorAll("body");
+let skill = document.querySelectorAll(".frame");
+let view = document.getElementsByClassName("largeView");
+let closeView = document.getElementsByClassName("closeView");
+
+skill.forEach(function(e, i) {
+    skill[i].addEventListener("click", function(){
+        let j = this.children[1].getAttribute("src");
+        view[0].children[1].children[0].setAttribute("src", j);
+        view[0].classList.remove("-hide");
+        view[0].classList.add("-show");
+        body[0].setAttribute("style", "overflow: hidden;");
+    });
+}, this);
+
+closeView[0].addEventListener("click", function(){
+    body[0].setAttribute("style", "overflow: auto;");
+    view[0].scrollTop = 0;
+    view[0].classList.remove("-show");
+    view[0].classList.add("-hide");
+}, this);
+
+view[0].addEventListener("click", function(e){
+    if (e.target !== this)
+    return;
+
+    body[0].setAttribute("style", "overflow: auto;");
+    this.scrollTop = 0;
+    this.classList.remove("-show");
+    this.classList.add("-hide");
+}, this);
+
